6b75840c90846531fe8ca4fd111f35cccb85e714
delicalm <stormindustries22@outlook.com>
2026-08-06 12:47:51 -0500

    Adding math functions

--- math_helper_by_delica/math_helper_funcs.py
+++ math_helper_by_delica/math_helper_funcs.py
@@ -260,6 +260,57 @@ def add_vals_with_sig_figs(val_1, val_2, val_1_prec=-1, val_2_prec=-1):
         num_sig_figs = get_num_sig_figs(unrounded_result, bigger_prec)
     return rounded_sum, num_sig_figs
 
+def add_list_vals_with_sig_figs(val_list, precisions=-1):
+    """Calculates the sum of all values in a list rounded to the correct number of significant figures.
+
+    Parameters
+    ----------
+    val_list : list[int | float]
+    val_1 : int | float
+        The first value in the sum.
+    val_2 : int | float
+        The second value in the sum.
+    val_1_prec : int | float, default -1
+        The precision of the first value (1 for value rounded to the ones place, 10 for value rounded to the tens
+        0.1 for value round to the tenths place, etc.). If the first value is a constant with an infinite precision,
+        please use the default precision of -1.
+    val_2_prec : int | float, default -1
+        The precision of the second value (1 for value rounded to the ones place, 10 for value rounded to the tens
+        0.1 for value round to the tenths place, etc.). If the second value is a constant with an infinite precision,
+        please use the default precision of -1.
+
+    Returns
+    -------
+    rounded_sum : int | float
+        The sum of two values rounded to the correct number of significant figures.
+    num_sig_figs : int
+        The number of significant figures in the sum value. If the sum is a constant with an infinite precision,
+        this value will be -1 (because a constant theoretically has infinitely many significant figures).
+    """
+    error_lib.check_type(val_1, float, "Value 1 (in add with sig figs)", alt_type=int)
+    error_lib.check_type(val_2, float, "Value 2 (in add with sig figs)", alt_type=int)
+    error_lib.check_type(val_1_prec, float, "Value 1 Precision (in add with sig figs)", alt_type=int)
+    if val_1_prec != -1:
+        error_lib.check_value_is_positive(val_1_prec, "Value 1 Precision")
+    error_lib.check_type(val_2_prec, float, "Value 2 Precision (in add with sig figs)", alt_type=int)
+    if val_2_prec != -1:
+        error_lib.check_value_is_positive(val_2_prec, "Value 2 Precision")
+    unrounded_result = val_1 + val_2
+    rounded_sum = unrounded_result
+    num_sig_figs = -1
+    if val_1_prec != -1 or val_2_prec != -1:
+        bigger_prec = val_1_prec
+        if val_1_prec == -1:
+            bigger_prec = val_2_prec
+        if val_1_prec != -1 and val_2_prec != -1:
+            if val_1_prec > val_2_prec:
+                bigger_prec = val_1_prec
+            else:
+                bigger_prec = val_2_prec
+        rounded_sum = round_to_precision(unrounded_result, bigger_prec)
+        num_sig_figs = get_num_sig_figs(unrounded_result, bigger_prec)
+    return rounded_sum, num_sig_figs
+
 def mult_vals_with_sig_figs(val_1, val_2, val_1_prec=-1, val_2_prec=-1):
     """Calculates the product of two values rounded to the correct number of significant figures.
 
